413cd3d4676a43374d001c22efc8c7b705515946
sol_rosser <solomonj2k12@hotmail.co.uk>
2018-09-05 12:17:18 +0000

    created another py file to spread out code so files were not so full

--- gametext.py
+++ gametext.py
@@ -4,3 +4,35 @@ def add_eliminated_text(player):
     text_to_write = "{} has been eliminated".format(username)
     with open("gamefiles/eliminated.txt", "a") as f:
         f.writelines(text_to_write)
+        
+def add_correct_text(text):
+   
+    with open("gamefiles/correct.txt", "a") as f:
+        f.writelines("<span class='correct'>{}</span>".format(text))
+        
+def add_incorrect_text(text):
+   
+    with open("gamefiles/correct.txt", "a") as f:
+        f.writelines("<span class='incorrect'>{}</span>".format(text))
+        
+
+
+        
+def add_correct_answer_text(correct_answer):
+   
+
+    text_to_write = "The correct answer was: {0}<br>".format(correct_answer)
+    with open("gamefiles/answer.txt", "a") as f:
+        f.writelines(text_to_write)
+        
+def add_quiz_master_text(text):
+    
+    with open("gamefiles/quizmaster.txt", "a") as f:
+        f.writelines("{}<br>".format(text)) 
+        
+        
+def add_question_text(question):
+    
+    with open("gamefiles/question.txt", "a") as f:
+        f.writelines("{}<br>".format(question))
+        
